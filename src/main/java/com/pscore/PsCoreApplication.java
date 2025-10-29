a7ce59c7529d794fa028af482b3af0b20b61324c
natanPrs <natan.void@proton.me>
2025-10-29 16:27:58 -0300

    Initial commit

---
+++ src/main/java/com/pscore/PsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.pscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pscore/PsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
